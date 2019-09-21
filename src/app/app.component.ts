110e57ac624f5facd8f0218400e6b9cae3a89ceb
Nolawi Petros <nolawi.petros@gmail.com>
2019-09-21 15:36:30 -0400

    update initial loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { AnalyticsService } from './@core/utils/analytics.service';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'ngx-app',
   template: '<router-outlet></router-outlet>',
+  animations: [
+    // animation triggers go here
+  ],
 })
 export class AppComponent implements OnInit {
   constructor(private analytics: AnalyticsService) {}
